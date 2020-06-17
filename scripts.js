f9df2294d2edd1534311c9c62419f06c3d1fb3b0
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-17 09:26:37 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ $("#endDate").text("16-Jun-2021");
 $("#type").text("Two Wheeler");
 $("#registrationNumber").text("PB08AY1101");
 $("#make").text("BAJAJ");
-$("#model").text("DISCOVER 150");
+$("#model").text("PULSAR 150");
 $("#vehicalSubType").text("ES FA DI");
 $("#engineNo").text("DHGBNA62466");
 $("#chassis").text("MD2DHDHZZNCA25060");
